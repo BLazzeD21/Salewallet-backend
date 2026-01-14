7067ca53edef1bba9d784b4d02fce936febd3e72
Alexandr Sekerin <blazzed21@gmail.com>
2026-01-15 02:36:40 +0300

    feat: add CORS support and update dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,50 @@
+import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+
 import { connectDB, disconnectDB } from "./config/db.js";
 
-import userRoutes from './routes/userRoutes.js';
+import userRoutes from "./routes/userRoutes.js";
 
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+const DOMAIN = process.env.DOMAIN;
+
+const allowedOrigin =
+  process.env.NODE_ENV === "production" ? DOMAIN : `http://localhost:${PORT}`;
+
+const corsOptions = {
+  origin: allowedOrigin,
+  optionsSuccessStatus: 200,
+  credentials: true,
+};
 
 const main = async () => {
   try {
-    dotenv.config();
-
     await connectDB();
 
     const app = express();
     app.use(express.json());
+    app.use(cors(corsOptions));
 
-    app.get('/', (req, res) => res.send('Server is running'));
-    app.use('/api/v1', userRoutes);
+    app.get("/", (req, res) => res.send("Server is running"));
+    app.use("/api/v1", userRoutes);
 
-    const PORT = process.env.PORT || 5000;
-    const server = app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    const server = app.listen(PORT, () =>
+      console.log(`Server running on port ${PORT}`)
+    );
 
     const gracefulShutdown = async () => {
-      console.log('\nShutting down server');
+      console.log("\nShutting down server");
       server.close(async () => {
         await disconnectDB();
         process.exit(0);
       });
     };
 
-    process.on('SIGINT', gracefulShutdown);
-    process.on('SIGTERM', gracefulShutdown);
-
+    process.on("SIGINT", gracefulShutdown);
+    process.on("SIGTERM", gracefulShutdown);
   } catch (error) {
     console.error("Server failed to start:", error);
     process.exit(1);
