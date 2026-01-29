4d912b3a008dad5d4990a8fe31fb930bd72c0867
Alexandr Sekerin <blazzed21@gmail.com>
2026-01-29 17:05:45 +0300

    refactor: correct controller name

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,7 +2,7 @@ export * from "./changePassword.controller";
 export * from "./confirmEmail.controller";
 export * from "./createCard.controller";
 export * from "./deleteCard.controller";
-export * from "./deleteUser.contorller";
+export * from "./deleteUser.controller";
 export * from "./getCards.controller";
 export * from "./login.controller";
 export * from "./registration.controller";
